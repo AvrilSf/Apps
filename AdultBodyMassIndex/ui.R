67543cf12a6ca7a6b345605cf3db5d215b4f977d
AvrilSf <67452071+AvrilSf@users.noreply.github.com>
2020-11-30 21:14:47 -0600

    Add files via upload
    Corrected comments

---
+++ AdultBodyMassIndex/ui.R
@@ -0,0 +1,40 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Adult Body Mass Index"),
+
+    # Sidebar with a slider input and two check boxes
+    sidebarLayout(
+        sidebarPanel(
+          numericInput("numeric", "Your weight in pounds:",
+                       value = 120, min = 80, max = 1000, step = 1),  
+          sliderInput("height",
+                        "Your height in inches:",
+                        min = 58,
+                        max = 76,
+                        value = 58),
+          checkboxInput("show_BMI", "Show/Hide BMI Indicator", value = TRUE),
+          checkboxInput("show_comment", "Show/Hide Comment", value = TRUE)
+        ),
+
+        # Show outcome
+        mainPanel(
+          h3("BMI for 18 years and older"),  
+          textOutput("text1"),
+          textOutput("text2"),
+          textOutput("text3")
+        )
+    )
+))
